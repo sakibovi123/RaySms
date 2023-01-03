ba0cb773b10bb5f296656df095aeb4288cad79ed
unknown <sakibovi123@gmail.com>
2023-01-03 13:48:55 +0600

    stashes

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-//use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Http;
 use \GuzzleHttp\Psr7\Request;
-use Twilio\Rest\Client;
+use Twilio\Rest\Client as Cli;
+
 
 
 class CampaignController extends Controller
@@ -45,9 +47,9 @@ class CampaignController extends Controller
         );
 
         $campaigns = $client->sendAsync($request)->wait()->getBody();
-        foreach($campaigns["campaign"]["affiliateNumbers"] as $numb){
-            $this->send_message($numb);
-        }
+//        foreach($campaigns["campaign"]["affiliateNumbers"] as $numb){
+//            $this->send_message($numb);
+//        }
 
 
         // echo $campaigns;
@@ -59,19 +61,30 @@ class CampaignController extends Controller
     // sending message to numbers
     public function send_message($numbers)
     {
-        $twilio_sid = getenv("TWILIO_SID");
-        $twilio_token = getenv("TWILIO_TOKEN");
+        $twilio_sid = getenv("AC2b0cc7c783ccc1e82f3771636dda5e73");
+        $twilio_token = getenv("e224561207d5ebf64cfe5411da16fccb");
+
+
+        $message_content = array(
+            "We apologize that your call was dropped. We have qualified you for a simple, low-cost health insurance plan. Contact us at +18883470772 again within 1 minute to find out more about your free, no-risk health insurance plan!",
+            "Hi, Congratulations! As an apology for the dropped call, We are currently offering you a limited time 20% discount on the ACA Insurance Plan, just for you! Please reach us again at +18883470772. Grab the Lifetime HealthCare Opportunity within a Minute!",
+            "Still not Happy with our 20% discount? We have reserved a special enrollment opportunity just for you and your family (all-inclusive medical protection & 25% Discount)! Reduce your chances of becoming ill or injured with ACA, as 14.5 million US citizens have done. Call Now: +18883470772 before the offer expires Today!",
+            "We think you haven't noticed our previous offer yet. Don't worry, the offer is still available. There are more opportunities this time for you and your family to receive a 30% discount on ACA health insurance. Call Now: +18883470772 and secure your discounted ACA plan today!",
+            "We do not want to bother you, just following up about our ACA plan. We can help get your ACA health insurance fixed so your life is more manageable. If you apply today, you might be able to get free health insurance through the Marketplace. Call us +18883470772",
+            "Hey there! It’s our final follow-up from our end. Do you still wish to proceed? We are always here to assist you. Call us +18883470772 without any hesitation."
+        );
         if( count($numbers) > 0 )
         {
             foreach($numbers as $number)
             {
                 try
                 {
-                    $client = new Client($twilio_sid, $twilio_token);
+                    $client = new Cli($twilio_sid, $twilio_token);
                     $client->messages->create(
-                        $number, [
-                            "from" => "",
-                            "body" => ""
+                        $number,
+                        [
+                            "from" => "+18882998227",
+                            "body" => $message_content++
                         ]
                     );
 
