06f730513773cebca52540dc32ff3e6b49234147
unknown <sakibovi123@gmail.com>
2023-02-15 00:41:46 +0600

    latest commit

---
+++ resources/views/Dashboard/base.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <title>Ray Advertising SMS Platform</title> -->
+    <meta name="author" content="Nosia Akter">
+    <meta name="description" content="Ray Advertising SMS Platform">
+    <title>{{config('Ray Advertising SMS Platform', 'Ray Advertising SMS Platform')}} - @yield('title')</title>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/ray-logo.png') }}" />
+    <!-- Tailwind -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
+        .font-family-karla { font-family: karla; }
+        .bg-sidebar { background: #032738; }
+        .edit-trash { color: #032738; }
+        .cta-btn { color: #032738; }
+        .upgrade-btn { background: #1947ee; }
+        .upgrade-btn:hover { background: #0038fd; }
+        .active-nav-link { background: #248991; }
+        .icon-top { background: #248991; }
+        .icon-top-text { color: #248991; }
+        .login-text { color: #248991; }
+        .active-nav-text { color: #fff; }
+        .nav-item:hover { background: #248991; }
+        .account-link:hover { background: #3d68ff; }
+        .li { : #3d68ff; }
+    </style>
+</head>
+<body class="bg-gray-100 font-family-karla flex"></body>
